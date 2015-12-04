719dafd47634decfab68cf58f35b1b340e650918
manupaisable <manuelpais@gmail.com>
2015-12-04 17:09:12 +0100

    setup instructions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gocd-vagrant
+
+Run "vagrant up" after cloning the repo to spin up a VM with GoCD server (and 1 agent) installed.
+
+Then you should see Go's admin page (Add Pipeline) at this URL:
+
+http://localhost:8153/go
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 
 # install and start GoCD server and agent
-# source: http://www.go.cd/documentation/user/current/installation/install/server/linux.html#debian-based-distributions-ie-ubuntu
+# sources:
+# server - http://www.go.cd/documentation/user/current/installation/install/server/linux.html#debian-based-distributions-ie-ubuntu
+# agent  - https://www.go.cd/documentation/user/current/installation/install/agent/linux.html#debian-based-distributions-ie-ubuntu
 
 echo "deb http://dl.bintray.com/gocd/gocd-deb/ /" > gocd.list
 sudo mv gocd.list /etc/apt/sources.list.d/
@@ -9,7 +11,7 @@ wget --quiet -O - "https://bintray.com/user/downloadSubjectPublicKey?username=go
 sudo apt-get update
 sudo apt-get install -y go-server
 
-sudo etc/init.d/go-server start &
+sudo /etc/init.d/go-server start &
 
 # agent
 wget --quiet -O - "https://bintray.com/user/downloadSubjectPublicKey?username=gocd" | sudo apt-key add -
