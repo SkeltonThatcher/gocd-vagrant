b9a74bbf652e3b2ae0491fddc3ab034f9630a883
manupaisable <manuelpais@gmail.com>
2016-02-26 13:48:06 +0100

    closes issue #1

--- pipelines/scripts/launch_rails_aux.sh
+++ pipelines/scripts/launch_rails_aux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rake db:setup && rake assets:precompile && rails server -d -b 0.0.0.0 > /dev/null 2>&1
+. ~/.bash_profile && rake db:setup && rake assets:precompile && rails server -d -b 0.0.0.0 > /dev/null 2>&1
